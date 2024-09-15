55eddbf172a738cb7267be30de35326ba20c48f5
kalokoh <77471540+kalokoh@users.noreply.github.com>
2024-09-15 09:34:44 +0000

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,18 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA7baCfuTLP8RNLb6bvuQYdtj6pSr-DsMI",
-  authDomain: "tech101-81c8d.firebaseapp.com",
-  projectId: "tech101-81c8d",
-  storageBucket: "tech101-81c8d.appspot.com",
-  messagingSenderId: "678016670566",
-  appId: "1:678016670566:web:326a95c4e457b0fddf86a7"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const database = getFirestore(app);
-export default app;
 
